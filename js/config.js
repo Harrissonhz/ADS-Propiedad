0cba0da6b8057910ff71adb475f2a8a2843daea5
Harrisson Zapata <harrissonhzs@gmail.com>
2026-03-16 14:55:16 -0500

    Restaurar config.js y remover del gitignore para uso en Github Pages

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.ADS_CONFIG = {
+    supabaseUrl: 'https://chvwtewnfliajuuzysts.supabase.co',
+    supabaseAnonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNodnd0ZXduZmxpYWp1dXp5c3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MzUxMjAsImV4cCI6MjA4ODExMTEyMH0._MM0l8hBzu3rGOEpMbizeZzxITU3N5SjOtSS0TKMz7c',
+};
